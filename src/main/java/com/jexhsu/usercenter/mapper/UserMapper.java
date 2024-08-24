dd51024233d80cc5aaa3b8a27cf1543c939494c9
JexHsu <jexhsu@gmail.com>
2024-08-25 05:02:41 +0800

    ✨ feat(database): configure MySQL and add MyBatis-Plus

---
+++ src/main/java/com/jexhsu/usercenter/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.jexhsu.usercenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jexhsu.usercenter.model.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
--- src/test/java/com/jexhsu/usercenter/UserCenterApplicationTests.java
+++ src/test/java/com/jexhsu/usercenter/UserCenterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.jexhsu.user_center_backend;
+package com.jexhsu.usercenter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserCenterBackendApplicationTests {
+class UserCenterApplicationTests {
 
 	@Test
 	void contextLoads() {
